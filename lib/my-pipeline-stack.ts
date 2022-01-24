ef4967d4e88a219b9302f2dd80a209a56b002115
slungelongcobo <slungelongcobo@gmail.com>
2022-01-24 13:19:37 +0000

    initial commit

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -10,7 +10,7 @@ export class MyPipelineStack extends Stack {
       const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        input: CodePipelineSource.gitHub('slungelongcobo/cdk-test', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
